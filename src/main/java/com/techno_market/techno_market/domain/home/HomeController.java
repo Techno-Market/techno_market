0ebd15dc667843586c0c24eb884ebec731e6db28
jayjhseo <jayjhseo@gmail.com>
2024-01-16 17:58:28 +0900

    setting

---
+++ src/main/java/com/techno_market/techno_market/domain/home/HomeController.java
@@ -0,0 +1,14 @@
+package com.techno_market.techno_market.domain.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "메인 페이지";
+    }
+}
