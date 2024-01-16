ac19c8134d496bf0a1911e26db39934a5999282b
jayjhseo <jayjhseo@gmail.com>
2024-01-16 18:07:12 +0900

    1

--- src/main/java/com/techno_market/techno_market/domain/home/HomeController.java
+++ src/main/java/com/techno_market/techno_market/domain/home/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    //테스트
     @GetMapping("/")
     @ResponseBody
     public String home() {
