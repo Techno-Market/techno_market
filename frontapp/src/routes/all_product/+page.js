12a6e84fd90a84f3fb3e80ed5e4d3db2a6064f3c
ko <taengja18@naver.com>
2024-01-31 11:20:19 +0900

    다건조회 완료

---
+++ frontapp/src/routes/all_product/+page.js
@@ -0,0 +1,7 @@
+export async function load ({fetch}) {
+
+    let res = await  fetch('http://localhost:8080/api/articles')
+    let result = await res.json();
+
+    return result;
+}
--- frontapp/src/routes/all_product/+page.svelte
+++ frontapp/src/routes/all_product/+page.svelte
@@ -1,267 +1,40 @@
+<script>
+    export let data;
+</script>
 <div class="sub-cnt-area w100per rel zi1">
     <div class="con w100per">
         <h1 class="title-text lh120 tb">전체 상품</h1>
         <ul class="product-box flex fww">
+            {#each data.data.articles as article}
             <li>
-                <a href="">
+                <a href="/sales_post/detail/{article.id}">
+                    <!-- {#each article.photo as photo} -->
+                    {#if article.photo && article.photo[0]}
                     <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
+                        <img src={`http://localhost:8080/gen/${article.photo[0].filePath}`} alt={article.photo[0].origFileName}>
                         <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
                             <img src="/img/ico_heart.svg" alt="">
                         </button>
                     </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
+                    {/if}
+                    <!-- {/each} -->
+                    <h3 class="c222 mt20 f16 tal">{article.subject}</h3>
                     <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
+                        <h4 class="c222 f18 tb">{article.price}</h4>
                         <span class="c999 f14">1일전</span>
                     </div>
                     <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
+                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">{article.area}</li>
+                        {#if article.directly}
                         <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
+                        {/if}
+                        {#if article.parcel}
                         <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
+                        {/if}
                     </ul>
                 </a>
             </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <div class="img-box rel">
-                        <img src="/img/product_img_1_1.jpg" alt="">
-                        <button class="favor-box img-box w24 abs" id="favor_btn" style="top: 12px; right: 12px;">
-                            <img src="/img/ico_heart.svg" alt="">
-                        </button>
-                    </div>
-                    <h3 class="c222 mt20 f16 tal">아이폰14 프로 128GB</h3>
-                    <div class="mt12 flex aic jcsb">
-                        <h4 class="c222 f18 tb">320,000원</h4>
-                        <span class="c999 f14">1일전</span>
-                    </div>
-                    <ul class="mt20 flex g4">
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b005DE8 cfff">대전</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">직거래</li>
-                        <li class="f13 bdr4 bsb pt4 pb4 pl8 pr8 b00A71B cfff">택배거래</li>
-                    </ul>
-                </a>
-            </li>
+            {/each}
         </ul>
     </div>
 </div>
